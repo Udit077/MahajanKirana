7a6b80f064a27495ef452b2cbb097d99bbbad3a5
Udit <uditmahajanmhs@gmail.com>
2021-04-20 23:00:33 +0530

    first

--- Manager/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Series,Bill,Notifications,Order, Uploadedlist, Customer
-# Register your models here.
-admin.site.register(Series)
-admin.site.register(Notifications)
-admin.site.register(Order)
-admin.site.register(Bill)
-admin.site.register(Uploadedlist)
-admin.site.register(Customer)
